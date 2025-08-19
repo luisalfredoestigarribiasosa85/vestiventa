c729ae30a1efbc53c0f4910dbede9e663ff676e6
Luis Estigarribia <luisalfredoestigarribiasosa85@gmail.com>
2025-08-18 22:41:41 -0300

    more changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
-
-bin/rails db:migrate
+set -euxo pipefail
+ruby -v
+bundle -v
+bundle install --without development test
+# Precompilación (si falla aquí, verás el mensaje real)
+RAILS_ENV=production bundle exec rails assets:precompile
+# Migraciones (si falla aquí, es tema de DB/migraciones)
+RAILS_ENV=production bundle exec rails db:migrate
